8f9efc8a66d738238c8677cf127028c76fe164b1
laetitiap <laetitia.pham@polytechnique.edu>
2014-10-23 19:45:13 +0200

    commit1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestGit::Application.config.secret_token = 'c0ae1ddbdcb403f1323defdb74181f66034f31d2587ca5c3bce3769df786e9a78a07b094c162461f60b59f39acc04ab8b738cdd608a5730eccbb01f39d251f1f'
